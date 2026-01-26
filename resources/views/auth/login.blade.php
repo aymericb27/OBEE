4fb059bbe730469d320929e3e6ed781007b71998
aymeric <aymeric.baurain@gmail.com>
2026-01-26 16:21:17 +0100

    traduction register / login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Mail')" />
             <x-text-input id="email" class="block p-2 mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mot de passe')" />
 
             <x-text-input id="password" class="p-2 block mt-1 w-full"
                             type="password"
@@ -28,7 +28,7 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600">{{ __('Se souvenir de moi') }}</span>
             </label>
         </div>
 
@@ -40,7 +40,7 @@
             @endif
 
             <x-primary-button class="ms-3 btn-primary">
-                {{ __('Log in') }}
+                {{ __('Se connecter') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,14 +4,14 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nom')" />
             <x-text-input id="name" class="block mt-1 w-full p-2" type="text" name="name" :value="old('name')"
                 required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
         <!-- FirstName -->
         <div>
-            <x-input-label for="firstname" :value="__('Firstname')" />
+            <x-input-label for="firstname" :value="__('Prénom')" />
             <x-text-input id="firstname" class="block mt-1 w-full p-2" type="text" name="firstname" :value="old('firstname')"
                 required autofocus autocomplete="firstname" />
             <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
@@ -19,14 +19,14 @@
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Mail')" />
             <x-text-input id="email" class="block mt-1 w-full p-2" type="email" name="email" :value="old('email')"
                 required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
         <!-- University -->
         <div class="mt-4">
-            <x-input-label for="university_id" :value="__('University')" />
+            <x-input-label for="university_id" :value="__('Université')" />
 
             <select id="university_id" name="university_id"
                 class="block mt-1 w-full p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
@@ -47,7 +47,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mot de passe')" />
 
             <x-text-input id="password" class="block mt-1 p-2 w-full" type="password" name="password" required
                 autocomplete="new-password" />
@@ -57,7 +57,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmer le mot de passe')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full p-2" type="password"
                 name="password_confirmation" required autocomplete="new-password" />
@@ -68,11 +68,11 @@
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Déjà enregistré ?') }}
             </a>
 
             <x-primary-button class="ms-4 btn-primary">
-                {{ __('Register') }}
+                {{ __("S'enregistrer") }}
             </x-primary-button>
         </div>
     </form>
